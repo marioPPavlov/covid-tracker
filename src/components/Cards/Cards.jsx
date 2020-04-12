ac275c6d58b973771afd37f1c71eb0ea310c0d77
marioPPavlov <mario.pavlov.pavlov@gmail.com>
2020-04-12 08:14:34 +0300

    added a working death/infected chart

--- src/components/Cards/Cards.jsx
+++ src/components/Cards/Cards.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Card, CardContent, Typography, Grid, StylesProvider } from "@material-ui/core"
+import { Card, CardContent, Typography, Grid } from "@material-ui/core"
 import CountUp from "react-countup"
 import styles from "./Cards.module.css";
 import cx from "classnames"
@@ -39,7 +39,7 @@ const Cards = ({ data: { confirmed, recovered, deaths, lastUpdate } }) => {
 
         <Grid item component={Card} xs={12} md={3} className={cx(styles.card, styles.deaths)}>
           <CardContent>
-            <Typography color="textSecondary" gutterBottom>Infected</Typography>
+            <Typography color="textSecondary" gutterBottom>Deaths</Typography>
             <Typography variant="h5">
               <CountUp start={0} end={deaths.value} duration={2.5} separator="," />
             </Typography>
